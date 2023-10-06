f253e9be5ef0ade04e8608f271be52864ad9039f
Varad Kulkarni <varadkulkarni172@gmail.com>
2023-10-06 10:41:57 +0530

    Src files have been added

---
+++ MAX30100_PublicDefine.h
@@ -0,0 +1,12 @@
+#ifndef MAX30100_PUBLIC_DEFINE_H
+#define MAX30100_PUBLIC_DEFINE_H
+
+// LED Current
+enum LEDCurrent {
+  LEDCurrent_5mA = 0x01,
+  LEDCurrent_10mA = 0x02,
+  LEDCurrent_20mA = 0x03,
+  LEDCurrent_40mA = 0x04,
+};
+
+#endif
